f8f71cbde86f901beda534e082d09f421d155e07
ashish130400 <71765780+ashish130400@users.noreply.github.com>
2022-03-09 20:35:54 +0530

    Add files via upload

---
+++ first.cpp
@@ -0,0 +1,28 @@
+
+#include <iostream>
+using namespace std;
+int main() {
+
+
+	cout << "Ashish  \t C++ \n assignment 1\n";
+
+	//std::cin>> a;
+	
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//1)	Write a program to print “Hello World” using escape sequences: \n, \t, \r, \”, \b
